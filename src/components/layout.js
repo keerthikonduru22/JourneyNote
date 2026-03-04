93a499ca50314ed65bacdfae4c34554e3ca54105
keerthikonduru22 <keerthikonduru04@gmail.com>
2026-03-04 23:42:08 +0530

    Update layout and remove weekllyplan

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,38 +1,39 @@
 import React from 'react';
-import { createHashRouter, RouterProvider, Link } from 'react-router-dom';
+import { createHashRouter, RouterProvider, Link, Outlet } from 'react-router-dom';
 import './layout.css'; 
 import Todaylist from './todaylist';
 import WeeklyPlan from './weeklyplan';
 import Teamplanning from './teamplanning';
 
-export default function Layout() {
-  const router = createHashRouter([
-    {
-      path: "/",
-      element: <Todaylist />
-    },
-    {
-      path: "/weeklyplan",
-      element: <WeeklyPlan />
-    },
-    {
-      path: "/Teamplanning",
-      element: <Teamplanning />
-    }
-  ]);
-
-    return (
-      <div>
-    <div className='container'>
-      <ul className='first'>
-        <li><Link to="/">Today list</Link></li>
-        <li><Link to="/weeklyplan">Weekly plan</Link></li>
-        <li><Link to="/Teamplanning">Team planning</Link></li>
-                </ul>
-    </div>
+// A simple layout component that renders the navigation and an Outlet
+function AppLayout() {
+  return (
+    <div>
+      <div className='container'>
+        <ul className='first'>
+          <li><Link to="/">Today list</Link></li>
+          <li><Link to="/weeklyplan">Weekly plan</Link></li>
+          <li><Link to="/Teamplanning">Team planning</Link></li>
+        </ul>
+      </div>
       <div className='main-content'>
-        <RouterProvider router={router} />
+        <Outlet />
       </div>
     </div>
   );
+}
+
+export default function Layout() {
+  const router = createHashRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        { path: '/', element: <Todaylist /> },
+        { path: '/weeklyplan', element: <WeeklyPlan /> },
+        { path: '/Teamplanning', element: <Teamplanning /> }
+      ]
+    }
+  ]);
+
+  return <RouterProvider router={router} />;
 }
